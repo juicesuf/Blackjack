123fbcb4df3bbf91201aeeecead6cd6133d33fe0
juicesuf <yusuftahamercimek@gmail.com>
2026-01-05 11:53:00 +0100

    Add project files.

---
+++ Blackjack.Lib/CardSuit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack.Lib
+{
+    public enum CardSuit
+    {
+        Club,
+        Spade,
+        Heart,
+        Diamond
+    }
+}
